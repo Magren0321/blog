4fc1677db21f406e5c338cc7f95482f67a2ac8cf
朱恒霖 <zhuhenglin21@gmail.com>
2023-10-19 13:53:17 +0800

    fix: init project

--- app/api/favicon/route.tsx
+++ app/api/favicon/route.tsx
@@ -62,7 +62,7 @@ export async function GET(req: NextRequest) {
     return NextResponse.error()
   }
 
-  let iconUrl = 'https://cali.so/favicon_blank.png'
+  let iconUrl = '/favicon_blank.png'
 
   try {
     const predefinedIcon = getPredefinedIconForUrl(url)
